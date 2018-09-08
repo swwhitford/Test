6483139909ead21475b349664f27004cda149481
Steve Whitford <swwhitford@yahoo.com>
2018-09-07 22:08:01 -0400

    l

--- HelloWorld/src/hello.java
+++ HelloWorld/src/hello.java
@@ -2,7 +2,7 @@
 public final class hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stubhhhhh
+		// TODO Auto-generated method 
 		int x=90;
 System.out.printf("Hello5 %d", x);
 	}
